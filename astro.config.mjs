694212f4fac7ea58a17670e8afefcfd75cefeaae
Tomeksy <165577906+Tomeksy@users.noreply.github.com>
2025-06-12 19:55:59 +0200

    Implement SEO optimizations: sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'static',
   adapter: netlify(),
   site: 'https://endearing-lily-dade62.netlify.app',
