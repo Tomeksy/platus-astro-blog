13ca20c7e934c8d7236ff6b8271ddedba4061ce5
Tomeksy <165577906+Tomeksy@users.noreply.github.com>
2025-06-12 16:46:42 +0200

    Updated package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
+  adapter: netlify(),
   site: 'https://endearing-lily-dade62.netlify.app',
   server: {
     host: '0.0.0.0',
