74e80d5d302b7f6e50ccc51be387dddc5c90dd19
Tomeksy <165577906+Tomeksy@users.noreply.github.com>
2025-06-12 08:30:20 +0200

    Convert to Static Site Generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
-import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   site: 'https://endearing-lily-dade62.netlify.app',
   image: {
     service: {
